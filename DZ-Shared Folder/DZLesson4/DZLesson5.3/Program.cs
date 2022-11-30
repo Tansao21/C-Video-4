28b8cfb508bd2f325601056a46c83988f9ee421c
Николай Мягков <tansao26@gmail.com>
2022-11-30 16:46:03 +0300

    DZLesson5.2 in DZLesson5.3

--- DZ-Shared Folder/DZLesson4/DZLesson5.3/Program.cs
+++ DZ-Shared Folder/DZLesson4/DZLesson5.3/Program.cs
@@ -15,25 +15,12 @@ do
 	i++;
 } while (i < num);
 
-	if (sum > 10)
+	if (sum >= 10)
 	{
-		Console.WriteLine("Сумма  цифр больше 10");
+		Console.WriteLine("Сумма  цифр больше 10 или равна 10");
 	}
 	else
 	{
 		Console.WriteLine("Сумма  цифр НЕ больше 10");
 	}
 
-/*	int n, s = 0;
-	Console.Write("Введите натуральное число: ");
-	n = Convert.ToInt32(Console.ReadLine());
-	while (n != 0)
-	{
-		s = s + n % 10;
-		n = n / 10;
-	}
-	if (s > 10)
-		Console.WriteLine("Сумма  цифр больше 10");
-	else
-		Console.WriteLine("Сумма  цифр НЕ больше 10");
-	Console.ReadKey();*/
