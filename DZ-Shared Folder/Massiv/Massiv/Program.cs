9fadf2b688e8d39a9d177f556cca410e18e145f7
Николай Мягков <tansao26@gmail.com>
2022-12-07 16:48:42 +0300

    Massiv 1.2

--- DZ-Shared Folder/Massiv/Massiv/Program.cs
+++ DZ-Shared Folder/Massiv/Massiv/Program.cs
@@ -12,6 +12,8 @@ for (int i = 0; i < 12; i++)
 }*/
 
 
+
+
 // Заполнит массивиз 20 элементов 20,19,18,...1.
 
 /*int[] massiv= new int[20];
@@ -19,11 +21,17 @@ for (int i = 0; i < 12; i++)
 for (int i = 0; i < 20; i++) massiv[i] = 19 + 1 - i;
 for (int i = 0; i < 20; i++) Console.WriteLine(massiv[i]);*/
 
+
+
+
 //Составить программу вывода на экран любого элемента массива по индексу. С этим заданием не уверен что праильно понял как решать!!!!!!!
 
 /*int[] massis = {1,2,3,4,5,6,7,8,9};
 
  Console.WriteLine(massis[8]);*/
+
+
+
 
 // Вывести элементы мвссива в обратном порядке.
 
@@ -32,13 +40,15 @@ for (int i = 0; i < 20; i++) Console.WriteLine(massiv[i]);*/
 for (int i = 0; i < massis.Length; i++) Console.WriteLine(massis[i] = massis.Length - i );*/
 
 
+
+
 // Дан массив. Напечтатьть: а) все неотрицательные элементы.
 
-int[] massis = { -1, 2, 3, -4, 5, 6, 7, -8, -9 };
+/*int[] massis = { -1, 2, 3, -4, 5, 6, 7, -8, -9 };
 for (int i = 0; i < massis.Length; i++)
 {
 	if (massis[i] > 0)
 	{
 		Console.WriteLine(massis[i]);
 	}
-}
+}*/
---
+++ DZ-Shared Folder/Massiv1.2/Massiv1.2/Program.cs
@@ -0,0 +1,92 @@
+﻿// Дан массив. Вывести на экран сначала его неотрицательные элементы, затем отрицательные.
+
+/*int[] num =  {1,2,-3,4,5,-8,90,23,-45,-67};
+
+for (int i = 0; i < num.Length; i++)
+{
+	if (num[i] > 0)
+	{
+		Console.WriteLine(num[i]);
+	}
+}*/
+
+/*for (int i = 0; i < num.Length; i++)
+{
+	if (num[i] < 0)
+	{
+		Console.WriteLine(num[i]);
+	}
+}*/
+
+
+
+//Дан массив целых чисел. Найти номера элементов оканчевающихся цифрой 0 ( известно что такие элементы в массиве есть)
+
+/*int[] mas= {10, 20,32,23,12,31,45,50,70,90,100 };
+
+for (int i = 0; i < mas.Length; i++)
+{
+	if (mas[i] % 10 == 0)
+	{
+		Console.WriteLine(i);
+	}
+}*/
+
+
+
+//Дан массив вещественных чисел. Каждый элемент, больший 10, заменить его квадратным корнем.
+
+
+int[] mas = { 0, 2, 3, 2, 12, 3, 45, 5, 70, 9, 100 };
+double num = 0;
+
+
+
+/*for (int i = 0; i < mas.Length; i++)
+{
+	if (mas[i] > 10)
+	{
+	 num = Math.Sqrt(mas[i]);
+	Console.WriteLine($"{i} {num}");
+	}
+}*/
+//for (int i = 0; i < mas.Length; i++) Console.WriteLine($"{i} { mas[i]}");
+
+
+// Дан массив. Найти: Сумму элементов массива, значение которыфх не привышает 20.
+
+/*int sum = 0;
+for (int i = 0; i < mas.Length; i++)
+{
+	sum += mas[i];
+	if (sum < 20)
+	{
+		Console.WriteLine($"{i}sum {sum}");
+	}
+}*/
+
+
+// Рост каждого из 22 учеников класса представлен в виде массиваю Определить количество учеников, рост которых не привышает значения r.
+
+int rost;
+int i = 0;
+int countStudent = 0;
+int[] rostStudent = new int [5];
+
+Console.WriteLine("Введите рост средний: ");
+rost= int.Parse(Console.ReadLine());
+
+
+do
+{
+	Console.WriteLine("Введите рост учеников: ");
+	rostStudent[i] = Convert.ToInt32(Console.ReadLine());
+
+	if (rostStudent[i] < rost)
+	{
+		countStudent ++;
+	}
+	i++;
+} while (i < 5);
+
+Console.WriteLine($" Колитчество учиников с ростом не привышает {rost} - {countStudent}");
