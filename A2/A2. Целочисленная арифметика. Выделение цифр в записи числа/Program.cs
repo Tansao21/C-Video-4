096f966e728bba41c5e068bf7bc3fe5d07318e26
Николай Мягков <tansao26@gmail.com>
2023-01-12 15:18:09 +0300

    Metodi

--- A2/A2. Целочисленная арифметика. Выделение цифр в записи числа/Program.cs
+++ A2/A2. Целочисленная арифметика. Выделение цифр в записи числа/Program.cs
@@ -6,49 +6,66 @@
 
 int num;
 bool a = true;
-try
+ void Input()
 {
-	do
-	{
 		Console.WriteLine("Введите двухзначное число: ");
 		num = int.Parse(Console.ReadLine());
+}
+
+static int Num(int num)
+{
 	if (num < 100 && num > 10)
 	{
 		Console.WriteLine("Ввели верно");
-			if (num <= 99 && num >= 10)
-			{
+		if (num <= 99 && num >= 10)
+		{
 
-				int desatki = num / 10;
-				int edinic = num % 10;
-				int sumEgoCifor = num % 10 + num / 10;
-				int proizvedenie = (num % 10) * (num / 10);
+			int desatki = num / 10;
+			int edinic = num % 10;
+			int sumEgoCifor = num % 10 + num / 10;
+			int proizvedenie = (num % 10) * (num / 10);
 
-				Console.WriteLine($"Число десяток в нем =  {desatki}"); 
-				Console.WriteLine($"Число единиц в нем = {edinic}");
-				Console.WriteLine($"Сумма его цифр = {sumEgoCifor}");
-				Console.WriteLine($"Произведение его цифр = {proizvedenie}");
+			Console.WriteLine($"Число десяток в нем =  {desatki}");
+			Console.WriteLine($"Число единиц в нем = {edinic}");
+			Console.WriteLine($"Сумма его цифр = {sumEgoCifor}");
+			Console.WriteLine($"Произведение его цифр = {proizvedenie}");
 
-				//A2 .2.Дано двузначное число.Получить число, образованное при перестановке цифр заданного числа.
-				Console.WriteLine();
+			//A2 .2.Дано двузначное число.Получить число, образованное при перестановке цифр заданного числа.
+			Console.WriteLine();
 
-				int numOne = edinic;
-				int numTwo = desatki;
-				Console.WriteLine($"{numOne}{numTwo}");
-			}
-		
-	a = false;
+			int numOne = edinic;
+			int numTwo = desatki;
+			Console.WriteLine($"{numOne}{numTwo}");
+		}
+
 	}
 	else if (num > 100 || num < 10)
 	{
 		Console.WriteLine("Неверное число");
 	}
+	return num;
+}
 
+void Output (string msg)
+{
+	Console.WriteLine(msg);
+}
 
+try
+{
+	Input();
+	do
+	{
+	Num(num);
+
+		a = false;
 	} while (a);
 }
 catch 
 {
-
-	Console.WriteLine(" Ввели не правильно! Введите 2х значное число!");
+	Output(" Ввели не правильно! Введите 2х значное число!");
 }
+
+
+
 
